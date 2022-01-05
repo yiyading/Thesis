6dc5346a20cfb55985f8d3aade18e4216724f170
YaDing Yi <yiyading@pku.edu.cn>
2022-01-05 15:13:34 +0800

    add D-latch and D-flipflop

--- Basics/Device.v
+++ Basics/Device.v
@@ -11,7 +11,7 @@
  * 
  */
 
-
+// FullAdder
 module FullAdder_1bit(
 	input wire a, b, cin,
 	output wire sum, cout
@@ -22,29 +22,84 @@ endmodule // FullAdder_1bit_behavior
 
 
 
-
-module Counter (
+// Counter
+module Counter_4bit (
 	input wire clk, rst,
-	output reg [2:0] cnt
+	output reg [3:0] cnt
 	);
 
-	parameter max = 3'b101;		// parameter常量参数化，增加代码易读性
-	always @(posedge rst, posedge clk)	// , 和 or 作用相同
+	parameter max = 4'b1010;					// parameter: improve code legibility
+	always @(posedge rst, posedge clk)		// , and or have the same effect
 		begin
 			if(rst)
 				begin
-					cnt <= 3'b000;
+					cnt <= 4'b0000;
 				end
 			else
 				begin
 					if(cnt == max)
 						begin
-							cnt <= 3'b000;
+							cnt <= 4'b0000;
 						end
 					else
 						begin
-							cnt <= cnt + 3'b001;
+							cnt <= cnt + 4'b0001;
 						end
 				end
 		end
 endmodule // Counter
+
+
+
+
+/*
+ *	Module:
+ *		D_latch();
+ *  	D_flipflop();		// posedge
+ */
+module D_latch(
+	input wire d, clk,
+	output reg q
+	);
+
+	always @(d, clk)
+		if(clk == 1)
+			q <= d;		// if clk=1 q capture d
+endmodule
+
+module D_flipflop(
+	input wire d, clk,
+	output reg q
+	);
+	
+	always @(posedge clk) 
+		begin
+			q <= d; 
+		end
+endmodule
+
+
+module D_flipflop_rst(
+	input wire d, clk, rst,
+	output reg q
+	);
+
+    reg q;//上电以后，在时钟没有工作之前，这个寄存器应该是个什么信号呢，就是初始化的信号
+          //通常上电初始化我们用复位信号来做，用复位信号来指定复位以后的状态
+//    always @ (posedge clk)//我们用正的时钟沿做它的敏感信号
+//    begin
+//        if(rst)//同步复位信号
+//            q <=0;
+//        else
+//            q <= d;//上升沿有效的时候，把d捕获到q
+//    end
+
+    always @ (posedge clk,posedge rst)//我们用正的时钟沿做它的敏感信号
+    begin
+        if(rst)//异步复位信号，跟时钟沿无关，只要复位是高电平就会复位
+            q <=0;
+        else
+            q <= d;//上升沿有效的时候，把d捕获到q
+    end    
+
+endmodule
--- Basics/Device_tb.v
+++ Basics/Device_tb.v
@@ -14,16 +14,23 @@
 `timescale 1ms/100ns
 `include "Device.v"
 
+/* ************************************************************************************************************
+ *
+ * TestBench: 
+ *	 FullAdder_tb();
+ *
+ * ************************************************************************************************************/
+
 module FullAdder_tb();
 	reg A_1bit, B_1bit, CIN_1bit, CLK;
 	wire SUM,COUT;
 
 	initial 
 		begin
-			$display("Running Gate module testbench");	
+			$display("Running module testbench");	
 			$dumpfile("Device.vcd");	// 打开一个VCD数据库用于记录
 			// $dumpvars(level,start_module); 要记录的信号，level=0表示记录所有
-			$dumpvars(0, FullAdder_1bit);					
+			$dumpvars(0, FullAdder_tb, Counter_tb, D_latch_tb, D_flipflop_tb);					
 			#50
 			A_1bit = 1'b1;
 			B_1bit = 1'b1;
@@ -44,6 +51,122 @@ module FullAdder_tb();
 		.sum(SUM),
 		.cout(COUT)
 		);
+
 	initial #1050 $finish;
 endmodule
+
+
+/* ************************************************************************************************************
+ *
+ * TestBench:
+ *	 Counter_tb();
+ *
+ * ************************************************************************************************************/
+
+module Counter_tb();
+    reg RST, CLK;
+    wire [3:0] CNT;
+
+    initial
+	    begin
+	    	CLK = 1'b0;
+	    	RST = 1'b1;
+	    	#50 RST = 1'b0;
+	    end
+	always #10 CLK <= ~CLK;
+
+    Counter_4bit Counter_4bit(
+    	.clk(CLK),
+    	.rst(RST),
+    	.cnt(CNT)
+    	);
+
+endmodule
+
+
+/* ************************************************************************************************************
+ *
+ * TestBench:
+ *	 D_latch_tb();
+ *	 D_flipflop_tb();
+ *
+ * ************************************************************************************************************/
+
+module D_latch_tb();
+    reg CLK, D;
+    wire Q;
+
+    initial
+	    begin
+	        CLK = 1;
+	        D <= 0;
+	        forever
+		        begin
+		        	/* 
+		        	 *	When CLK=1, Q=D, 
+		        	 *	change of D signal is also reflected in the output Q.
+		        	 *		-> generate glitches
+		        	 */
+		            #60 D <= 1;
+		            #22 D <= 0;
+		            #2  D <= 1;
+		            #2  D <= 0;
+		            // Keep low at 16ns and cycle periodically.
+		            #16 D <= 0;	
+		        end
+    	end
+
+	// full period of 40ns
+    always #20 CLK <= ~CLK;	
+
+    D_latch D_latch(
+    	.clk(CLK),
+    	.d(D),
+    	.q(Q)
+    	);
+
+endmodule
+
+module D_flipflop_tb();
+	reg CLK, D;
+	wire Q;
+
+	initial
+	    begin
+	        CLK = 1;
+	        D <= 0;
+	        forever
+		        begin
+		        	/* 
+		        	 *	When CLK=1, Q=D, 
+		        	 *	change of D signal isn't reflected in the output Q.
+		        	 *		-> can't generate glitches
+		        	 */
+		            #60 D <= 1; 
+		            #22 D <= 0;
+		            #2  D <= 1;
+		            #2  D <= 0;
+		            #16 D <= 0;
+		        end
+	    end
+
+	// full period of 40ns
+    always #20 CLK <= ~CLK;//半周期为20ns,全周期为40ns的一个信号
+
+    D_flipflop D_flipflop(
+    	.clk(CLK),
+    	.q(Q),
+    	.d(D)
+    	);
+endmodule
+
+
+
+
+
+
+
+
+
+
 
